4fce10d7adfbb765482f483aee81109704712faf
Andrew Bullock <trullock@gmail.com>
2021-07-30 07:49:01 +0100

    symlink jct test

---
+++ maze/lib/slave/slave.cpp
@@ -0,0 +1,88 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include "slave.h"
+
+Slave* Slave::self = nullptr;
+
+Slave::Slave(int i2cAddress) {
+	self = this;
+	this->i2cAddress = i2cAddress;
+
+	Wire.begin(this->i2cAddress);
+	// enable broadcasts to be received
+	TWAR = (this->i2cAddress << 1) | 1;
+
+	Wire.onRequest(Slave::reportStatusWrapper);
+	Wire.onReceive(Slave::receiveCommandWrapper);
+
+	Serial.print("Module joining I2C bus with address: ");
+	Serial.println(this->i2cAddress);
+}
+
+void Slave::reportStatusWrapper() {
+	self->reportStatus();
+}
+
+void Slave::receiveCommandWrapper(int x) {
+	self->receiveCommand(x);
+}
+
+void Slave::reportStatus() {
+	Wire.write(this->state);
+	Wire.write(this->strikes);
+	Wire.write(this->pendingNotification);
+	this->pendingNotification = 0;
+}
+
+void Slave::reportStrike() {
+	// The master will query this and relay it back as a Strike() instruction
+	this->strikes++;
+}
+
+void Slave::arm()
+{
+	Serial.println("Arming");
+}
+
+void Slave::explode()
+{
+	Serial.println("Exploding");
+	this->state = STATE_EXPLODING;
+}
+
+void Slave::strike()
+{
+	Serial.println("Striking");
+	this->state = STATE_STRIKING;
+}
+
+void Slave::deactivate()
+{
+	this->state = STATE_DEACTIVATED;
+}
+
+void Slave::setDifficulty(byte diff)
+{
+	Serial.print("Setting difficulty to: ");
+	Serial.println(diff);
+
+	this->difficulty = diff;
+}
+
+void Slave::receiveCommand(int howMany)
+{
+	// Master scanning for modules will cause this
+	if(howMany == 0)
+		return;
+	
+	byte command = Wire.read();
+
+	if(command == COMMAND_DIFFICULTY)
+		this->setDifficulty(Wire.read());
+	else if(command == COMMAND_ARM)
+		this->arm();
+	else if (command == COMMAND_STRIKE)
+		this->strike();
+	else if (command == COMMAND_EXPLODE)
+		this->explode();
+}
---
+++ maze/lib/slave/slave.h
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "../constants.h"
+
+class Slave {
+
+	static Slave* self;
+	static void reportStatusWrapper();
+	static void receiveCommandWrapper(int x);
+
+	void reportStatus();
+	void receiveCommand(int howMany);
+
+protected:
+	int i2cAddress = 0;
+
+	byte pendingNotification = 0;
+	byte strikes = 0;
+	byte state = STATE_DISARMED;
+	byte difficulty = 1;
+
+	virtual void reportStrike();
+
+	virtual void arm();
+	virtual void strike();
+	virtual void explode();
+	virtual void deactivate();
+	virtual void setDifficulty(byte difficulty);
+
+public:
+
+	Slave(int i2cAddress);
+	virtual void loop() = 0;
+};
